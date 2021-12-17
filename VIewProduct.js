f934087a91eee8734901ac36d24e99093976b09f
Gyaneshwar Kumar <codewithgyan@gmail.com>
2021-12-18 00:10:50 +0530

    .

--- VIewProduct.js
+++ VIewProduct.js
@@ -6,6 +6,10 @@ window.addEventListener("load",function(){
     let price=document.getElementById("price");
     let details=document.getElementById("details");
     let pro_img=document.getElementById("pro_img");
+    let pro_img_1=document.getElementById("pro_img_1");
+    let pro_img_2=document.getElementById("pro_img_2");
+    let pro_img_3=document.getElementById("pro_img_3");
+
     let data=JSON.parse(localStorage.getItem("currentlySearched"));
     if(data!=null)
     {
@@ -16,5 +20,8 @@ window.addEventListener("load",function(){
         price.innerHTML="Rs "+data.Price;
         details.innerHTML=data.Details;
         pro_img.setAttribute("src",data.Image);
+        pro_img_1.setAttribute("src",data.Image);
+        pro_img_2.setAttribute("src",data.Image);
+        pro_img_3.setAttribute("src",data.Image);
     }
 });
